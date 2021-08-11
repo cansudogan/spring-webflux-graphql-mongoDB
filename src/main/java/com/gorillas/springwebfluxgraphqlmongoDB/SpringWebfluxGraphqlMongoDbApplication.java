44cef65db7489b8a81a95e3086caef5ef25a64f8
cansudogan <cansudogan95@gmail.com>
2021-08-11 14:40:12 +0300

    first commit

---
+++ src/main/java/com/gorillas/springwebfluxgraphqlmongoDB/SpringWebfluxGraphqlMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.gorillas.springwebfluxgraphqlmongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxGraphqlMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxGraphqlMongoDbApplication.class, args);
+	}
+
+}
